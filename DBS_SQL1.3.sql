061fdb0c3263c89c1103b7f4c8f5050e44cd8712
Agam Singh <singh.agam0009@gmail.com>
2021-10-05 14:01:10 +1100

    task 3 done

--- DBS_SQL1.3.sql
+++ DBS_SQL1.3.sql
@@ -86,12 +86,61 @@ CREATE TABLE OrderLine (
     OrderDate DATE,
     Qty INT NOT NULL CHECK(Qty > 0),
     PRIMARY KEY (ItemID, ClientID, OrderDate),
-    FOREIGN KEY (ClientID, OrderDate) REFERENCES Orders(ClientID, OrderDate),
-    FOREIGN KEY (ItemID) REFERENCES MenuItem(ItemID)
+    FOREIGN KEY (ItemID) REFERENCES MenuItem(ItemID),
+    FOREIGN KEY (ClientID, OrderDate) REFERENCES Orders(ClientID, OrderDate)
+    
 );
 
 SELECT * FROM ORGANISATION 
 SELECT * FROM CLIENT
-SELECT * FROM ORDERS
 SELECT * FROM MENUITEM
+SELECT * FROM ORDERS
 SELECT * FROM ORDERLINE 
+
+-- ===================Task 3===================================
+
+INSERT INTO Organisation(OrgID, OrganisationName) VALUES
+ ('DODG', 'Dod & Gy Widget Importers'),
+ ('SWUT', 'Swinburne University of Technology');
+
+INSERT INTO CLIENT(ClientID, Name, Phone, OrgID) VALUES
+(12, 'James Hallinan', '(03)5555-1234', 'SWUT'),
+(15, 'Anh Nguyen', '(03)5555-2345', 'DODG'),
+(18, 'Karen Mok', '(03)5555-3456', 'SWUT'),
+(21, 'Tim Baird', '(03)5555-4567', 'DODG'),
+(28, 'Agampreet Singh', '(03)5555-4012', 'SWUT');
+
+INSERT INTO MENUITEM(ItemId, Description, ServesPerUnit, UnitPrice) VALUES
+(3214, 'Tropical Pizza - Large', 2,	$16.00),
+(3216, 'Tropical Pizza - Small', 1, $12.00),
+(3218, 'Tropical Pizza - Family', 4, $23.00),
+(4325, 'Can - Coke Zero', 1, $2.50),
+(4326, 'Can - Lemonade', 1, $2.50), 
+(4327, 'Can - Harden Up', 1, $7.50);
+
+INSERT INTO ORDERS(ClientID, OrderDate, DeliveryAddress) VALUES
+(12, '2021-09-20', 'Room TB225 - SUT - 1 John Street, Hawthorn, 3122'),
+(21, '2021-09-14', 'Room ATC009 - SUT - 1 John Street, Hawthorn, 3122'),
+(21, '2021-09-27', 'Room TB225 - SUT - 1 John Street, Hawthorn, 3122'),
+(28, '2021-09-20', 'Room TD224 - SUT - 1 John Street, Hawthorn, 3122'),
+(15, '2021-09-20', 'The George - 1 John Street, Hawthorn, 3122'),
+(18, '2021-09-30', 'Room TB225 - SUT - 1 John Street, Hawthorn, 3122');
+
+INSERT INTO ORDERLINE(ItemID, ClientID, OrderDate, Qty) VALUES
+(3216, 12, '2021-09-20', 2),
+(4326, 12, '2021-09-20', 1),
+(3218, 21, '2021-09-14', 1),
+(3214, 21, '2021-09-14', 1),
+(4325, 21, '2021-09-14', 4),
+(4327, 21, '2021-09-14', 2),
+(3216, 21, '2021-09-27', 1),
+(4327, 21, '2021-09-27', 1),
+(3218, 21, '2021-09-27', 2),
+(3216, 15, '2021-09-20', 2),
+(4326, 15, '2021-09-20', 1),
+(3216, 18, '2021-09-30', 1),
+(4327, 18, '2021-09-30', 1);
+
+
+-- ==========================Task 4=============================
+
